fd9a8288d4f904b0519016f65b054c27ef4282f6
alumno <alumno@Instituto.local>
2026-06-04 10:39:22 -0300

    commit

--- TrabajoPractico5/app.js
+++ TrabajoPractico5/app.js
@@ -1,3 +1,10 @@
-let numero1 = 5
-let numero2 = 10
 console.log(numero1 + numero1+numero2)
+let suma
+let division
+let multiplicacion
+let resta
+let numero1 = 12
+let numero2 = 20
+
+suma= numero1 + numero2
+division = numero2 / numero2
