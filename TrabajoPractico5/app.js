2d42ed07b2656b94ed56fb9af3b8ce9301461546
FelipeValladares234 <52017315@ivt.edu.ar>
2026-06-09 16:08:33 -0300

    commit tp5 1 9/6

--- TrabajoPractico5/app.js
+++ TrabajoPractico5/app.js
@@ -7,4 +7,9 @@ let numero1 = 12
 let numero2 = 20
 
 suma= numero1 + numero2
+console.log(suma= numero1 + numero2) 
+(Suma)
 division = numero2 / numero2
+console.log(division) (division = numero1 / numero2)
+multiplicacion = numero1 * numero2
+console.log(multiplicacion) (multiplicacion = numero1 * numero2)
